9f1de62236fb9d8eefa50020761dc57d74b6da95
Nathan Corn <ncorn17@gmail.com>
2015-09-27 17:17:03 -0400

    Documentation.

--- BoxBuilder/Interfaces/IBoxPointGenerator.cs
+++ BoxBuilder/Interfaces/IBoxPointGenerator.cs
@@ -5,6 +5,19 @@ namespace BoxBuilder
     // TODO: It would be nice if the box point genertor supported an inside and outside box model like CSS does. Would make it easier to make boxes that fit inside of something as well as that have something fit inside of it.
     internal interface IBoxPointGenerator
     {
+        /// <summary>
+        /// This method is used to make a box that is open or closed. This cannot generate slots for dividers.
+        /// </summary>
+        /// <param name="StartConfig"></param>
+        /// <param name="Box">Contains the dimensions of the box to be created.</param>
+        /// <param name="Material"></param>
+        /// <param name="MachineSettings"></param>
+        /// <param name="TabsX">Number of tabs in the X direction.</param>
+        /// <param name="TabsY">Number of tabs in the Y direction.</param>
+        /// <param name="TabsZ">Number of tabs in the Z direction.</param>
+        /// <param name="MakeBoxOpen">Determines if the top piece of the box is generated or not. 
+        /// If this is true the top edges of the front, back, left, and right pieces will be flat.</param>
+        /// <returns></returns>
         Dictionary<CubeSide, List<Point>> GeneratePoints(StartPositionConfiguration StartConfig, 
             IBoxSquare Box, 
             IMaterial Material, 
@@ -14,6 +27,21 @@ namespace BoxBuilder
             int TabsZ, 
             bool MakeBoxOpen);
 
+        /// <summary>
+        /// generates a box that is always open. The open end contains slots to place dividers.
+        /// </summary>
+        /// <param name="StartConfig"></param>
+        /// <param name="Box"></param>
+        /// <param name="Material"></param>
+        /// <param name="MachineSettings"></param>
+        /// <param name="TabsX"></param>
+        /// <param name="TabsY"></param>
+        /// <param name="TabsZ"></param>
+        /// <param name="SlotDepth"></param>
+        /// <param name="SlotCount"></param>
+        /// <param name="SlotAngle"></param>
+        /// <param name="SlotDirection"></param>
+        /// <returns></returns>
         Dictionary<CubeSide, List<Point>> GeneratePoints(StartPositionConfiguration StartConfig,
             IBoxSquare Box,
             IMaterial Material,
