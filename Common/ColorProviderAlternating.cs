1604661d16c9c0c7d858b284011fe4b52c2c7f03
Nathan Corn <ncorn17@gmail.com>
2015-09-27 21:44:32 -0400

    Breaking out the color providers into individual files.

---
+++ Common/ColorProviderAlternating.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace ColorProvider
+{
+    public sealed class ColorProviderAlternating : IColorProvider
+    {
+        Color CurrentColor;
+        Color FirstColor;
+        Color SecondColor;
+
+        public ColorProviderAlternating(Color Color1, Color Color2)
+        {
+            FirstColor = Color1;
+            SecondColor = Color2;
+        }
+
+        public ColorProviderAlternating()
+        {
+            FirstColor = Color.Red;
+            SecondColor = Color.Blue;
+        }
+
+        public Color GetColor()
+        {
+            if (CurrentColor == FirstColor)
+            {
+                CurrentColor = SecondColor;
+            }
+            else
+            {
+                CurrentColor = FirstColor;
+            }
+
+            return CurrentColor;
+        }
+    }
+}
