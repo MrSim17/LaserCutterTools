4402d1850963ace477f74dad25468b07d7c928e3
Nathan Corn <ncorn17@gmail.com>
2016-09-17 17:55:59 -0400

    Adding some comments.

--- Common/PolarPointDouble.cs
+++ Common/PolarPointDouble.cs
@@ -2,17 +2,28 @@
 {
     public struct PolarPointDouble
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="R">Radius</param>
+        /// <param name="A">Angle</param>
         public PolarPointDouble(double R, double A)
         {
             this.R = R;
             this.A = A;
         }
 
+        /// <summary>
+        /// Radius
+        /// </summary>
         public double R
         {
             get; set;
         }
 
+        /// <summary>
+        /// Angle
+        /// </summary>
         public double A
         {
             get; set;
